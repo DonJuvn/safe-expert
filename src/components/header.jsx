1cb72ec8745fa75144ffd232ddc7e2980d8a48e6
DonJuan <armvnov1ch@gmail.com>
2025-06-15 02:04:04 +0500

    corrected smooth scrolling and footer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
-import React from "react";import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
    return (
@@ -6,17 +7,19 @@ const Header = () => {
          <div className="container">
             <div className="header">
                <div className="logo">
-                  <img
-                     src={process.env.PUBLIC_URL + "img/logo.png"}
-                     alt="logo"
-                  />
+                  <Link to="/">
+                     <img
+                        src={process.env.PUBLIC_URL + "img/logo.png"}
+                        alt="logo"
+                     />
+                  </Link>
                </div>
                <div className="navigation">
-                  <Link></Link>
-                  <Link></Link>
-                  <Link></Link>
-                  <Link></Link>
-                  <Link></Link>
+                  <Link to="/">Главная</Link>
+                  <Link to="/about">О компании</Link>
+                  <Link to="/services">Услуги</Link>
+                  <Link to="/licences">Лицензии</Link>
+                  <a href="#footer">Контакты</a>
                </div>
             </div>
          </div>
