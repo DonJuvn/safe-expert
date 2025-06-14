1439edfe580d3bae7da7aec013dffe663910f168
DonJuan <armvnov1ch@gmail.com>
2025-06-15 04:15:49 +0500

    header responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,30 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
-   return (
-      <div id="header">
-         <div className="container">
-            <div className="header">
-               <div className="logo">
-                  <Link to="/">
-                     <img
-                        src={process.env.PUBLIC_URL + "img/logo.png"}
-                        alt="logo"
-                     />
-                  </Link>
-               </div>
-               <div className="navigation">
-                  <Link to="/">Главная</Link>
-                  <Link to="/about">О компании</Link>
-                  <Link to="/services">Услуги</Link>
-                  <Link to="/licences">Лицензии</Link>
-                  <a href="#footer">Контакты</a>
-               </div>
-            </div>
-         </div>
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div id="header">
+      <div className="container">
+        <div className="header">
+          <div className="logo">
+            <Link to="/">
+              <img
+                src={process.env.PUBLIC_URL + "img/logo.png"}
+                alt="logo"
+              />
+            </Link>
+          </div>
+
+          <div className={`navigation ${isOpen ? "open" : ""}`}>
+            <NavLink to="/" onClick={() => setIsOpen(false)}>Главная</NavLink>
+            <NavLink to="/about" onClick={() => setIsOpen(false)}>О компании</NavLink>
+            <NavLink to="/services" onClick={() => setIsOpen(false)}>Услуги</NavLink>
+            <NavLink to="/licences" onClick={() => setIsOpen(false)}>Лицензии</NavLink>
+            <a href="#footer" onClick={() => setIsOpen(false)}>Контакты</a>
+          </div>
+
+          <div className="burger" onClick={() => setIsOpen(!isOpen)}>
+            ☰
+          </div>
+        </div>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Header;
