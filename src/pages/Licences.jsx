9432bf946c5f769077324143bd0ac41c11336f8a
rubyazz <ersultan.abduvalov@gmail.com>
2025-06-15 20:55:37 +0500

    [last page]

--- src/pages/Licences.jsx
+++ src/pages/Licences.jsx
@@ -19,7 +19,25 @@ export default function Licences({ bgImage }) {
                </div>
             </div>
          </div>
-         
+         <div className="container">
+         <div className="licence-section">
+         <div className="licence-section__grid">
+            {[1, 2].map((_, i) => (
+               <div className="licence-card" key={i}>
+               <img
+                  src={`/img/licence${i + 1}.png`}
+                  alt={`Лицензия ${i + 1}`}
+                  className="licence-card__image"
+               />
+               <div className="licence-card__buttons">
+                  <button className="licence-card__btn licence-card__btn--view">Посмотреть</button>
+                  <button className="licence-card__btn licence-card__btn--download">⬇ Скачать</button>
+               </div>
+               </div>
+            ))}
+         </div>
+         </div>
+         </div>
       </div>
    );
 }
