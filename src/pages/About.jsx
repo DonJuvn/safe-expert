5cda8b89700832a879d69b7973ff9ed6702e15f4
DonJuan <armvnov1ch@gmail.com>
2025-06-15 20:05:08 +0500

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react";import { Link } from "react-router-dom";
 
 export default function About({ bgImage }) {
    return (
@@ -9,15 +8,32 @@ export default function About({ bgImage }) {
             <div className="container">
                <div className="text">
                   <h1>
-                  МЫ СОЗДАЕМ БЕЗОПАСНЫЕ УСЛОВИЯ ТРУДА С ВЫГОДАМИ ДЛЯ
-                  РАБОТОДАТЕЛЯ!
-               </h1>
-               <a href="#">Подробнее</a>
+                     МЫ СОЗДАЕМ БЕЗОПАСНЫЕ УСЛОВИЯ ТРУДА С ВЫГОДАМИ ДЛЯ
+                     РАБОТОДАТЕЛЯ!
+                  </h1>
+                  <a href="#">Подробнее</a>
                </div>
             </div>
          </div>
          <div className="container">
-            <div className="about"></div>
+            <div className="about">
+               <div className="top">
+                  <div className="left">
+                     <h1 id="title">О компании</h1>
+                     <p id="text">
+                        Учебный центр «SAFE EXPERT» был создан в 29 февраля 2016
+                        году и на сегодняшний день является одним из ведущей
+                        компанией по обучению и повышению квалификации рабочего
+                        персонала по промышленной безопасности. Мы помогаем
+                        предприятиям обеспечить безопасность их сотрудников и
+                        самого предприятия. В компании работают эксперты
+                        высокого уровня, которые владеют большим опытом.
+                     </p>
+                  </div>
+
+                  <img src={process.env.PUBLIC_URL + "img/gallery1.jpg"} alt="" />
+               </div>
+            </div>
          </div>
       </div>
    );
